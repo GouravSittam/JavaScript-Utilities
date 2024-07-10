4830fbba98b83d372752eb63f1239705f4ec0a45
Gourav Chaudhary <gouravsittam@gmail.com>
2024-07-10 13:19:08 +0530

    new

---
+++ Datatypes.js
@@ -0,0 +1,14 @@
+//primitive and non primitive Datatypes in JavaScript
+
+let age=20;//number datatype
+let name="grv";//string datatype
+let isMale=true;//boolean datatype, true or false values only
+let x;//undefined datatype
+let y=null;//null datatype
+BigInt(1);//bigint datatype
+Symbol("grv");//symbol datatype
+
+
+//non primitive Datatypes in JavaScript
+//objects, arrays, functions are non primitive Datatypes in JavaScript. 
+
--- variables.js
+++ variables.js
@@ -2,3 +2,8 @@ fullName="Gourav Chaudhary";
 age=20;
 console.log(fullName);
 console.log("My age is "+age+" years");
+
+
+
+let myAge=20;
+console.log(myAge);
