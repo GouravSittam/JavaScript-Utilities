4e185caf75423bd3cf8111986a7cae0ca6bca649
Gourav Chaudhary <gouravsittam@gmail.com>
2024-07-13 15:20:52 +0530

    new

--- practice3.js
+++ practice3.js
@@ -8,8 +8,17 @@ for(let i=0;i<=100;i++){
 }
 
 //odd number
-for(let i=0;i<=100;i++){
-    if(i%2!==0){
-    console.log("num = " + i ); 
+for(let j=0;j<=100;j++){
+    if(j%2!=0){
+    console.log("num = " + j ); 
 }
 }
+
+//2
+
+let gamenum=22;
+let usernum=prompt("Guess the number: ");
+while(usernum!=gamenum){
+    usernum=prompt("you guess wrong number.Guess it again");
+}
+console.log("congratulation");
