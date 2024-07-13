955d76d62fb54ff1fec47f0e3500210b9a9ea388
Gourav Chaudhary <gouravsittam@gmail.com>
2024-07-13 15:31:39 +0530

    new

---
+++ Strings.js
@@ -0,0 +1,25 @@
+//strings
+let str="Shavi";
+let str2='D';
+console.log(str,str2);
+console.log(str.length,str2);
+
+
+let d="Shavii";
+console.log(d[2]);
+
+
+//template literals{Backticks}
+let g=`hey my name is gourav chaudhary`;
+console.log(typeof g);
+console.log(g);
+
+let obj={
+    item:"pen",
+    price:100,
+};
+console.log(`the cost of ${obj.item} is ${obj.price} ruppes`);
+
+
+
+
