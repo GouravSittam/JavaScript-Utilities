5592a43ed8e96395770a26fd72fa05801a7c2379
Gourav Chaudhary <gouravsittam@gmail.com>
2024-07-13 14:31:57 +0530

    new

--- loop.js
+++ loop.js
@@ -4,8 +4,15 @@ for(let i=1;i<=10;i++){
 } 
 console.log("loop has ended");
 
+// let sum=0;
+// for(let i=1;i<=5;i++){
+//     sum=sum+i;
+// }
+// console.log("Sum is "+sum);
+
 let sum=0;
-for(let i=1;i<=5;i++){
+let n=100;
+for(let i=1;i<=n;i++){
     sum=sum+i;
 }
 console.log("Sum is "+sum);
