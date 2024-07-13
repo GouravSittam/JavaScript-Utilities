83054743d17242725b0d2ff9add26831f66e3e3e
Gourav Chaudhary <gouravsittam@gmail.com>
2024-07-13 15:00:31 +0530

    data

--- loop.js
+++ loop.js
@@ -23,5 +23,42 @@
 // }
 
 
+//While loop
+// let i=1;
+// while(i<=5){
+//     console.log('i is ',i);
+//     i++;
+// }
 
 
+//do while loop
+// let j=20;
+// do{
+//     console.log("grv chaudhary");
+//     j++;
+// }while(j<=30)
+
+//for of loop for use string and arrys
+
+// let str="Gourav Chaudhary";
+
+// let size=0;
+// for(let i of str){
+//     console.log("i is ",i);
+//     size++;
+// }
+// console.log("String size  is "+size);
+
+
+//for in loop for use object and arrys
+let stu={
+    name:"Grv Chaudhary",
+    age:20,
+    cgpa:7.5,
+    ispass:"true",
+
+};
+for(let key in stu){
+    console.log("key=",key,"value=",stu[key]);
+
+}
